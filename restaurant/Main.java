c60961f26a99a58334a61e0182f1c7dee8ff6763
bahar-j <joonie811@naver.com>
2021-02-04 11:38:39 +0900

    fix: to share constant SCANNER throughout classes

--- restaurant/Main.java
+++ restaurant/Main.java
@@ -20,13 +20,13 @@ public class Main {
 			System.out.println("***************************************");
 			System.out.println("어떤 작업을 하시겠습니까? ");
 
-			int input = SCANNER.nextInt();
+			String input = SCANNER.nextLine();
 
 			switch(input) {
-				case 1:
+				case "1":
 					worker.addOrder();
 					break;
-				case 2:
+				case "2":
 					try {
 						int amount = worker.serveFood();
 						saleBook.add(amount);
@@ -34,7 +34,7 @@ public class Main {
 						System.out.println(e.getMessage());
 					}
 					break;
-				case 3:
+				case "3":
 					System.out.printf("매출액 총액은 %d입니다", saleBook.getTotalSales());
 					System.out.println();
 					break;
