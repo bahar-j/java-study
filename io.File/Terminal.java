bc89b21c6e120781c44c7832a143ddafd24416d4
bahar-j <joonie811@naver.com>
2021-02-05 17:19:19 +0900

    refactor: make error phrase as constant

--- io.File/Terminal.java
+++ io.File/Terminal.java
@@ -7,12 +7,15 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Terminal {
+public class DOSmain {
 	static final Scanner SCANNER = new Scanner(System.in);
-	
+	static final String NO_SUCH_DIRECTORY = "cd: no such file or directory: ";
+	static final String NO_SUCH_COMMAND = "zsh: command not found: ";
+	static final String HOME = "{$HOME}";
+
 	public static void ls(File file) {
 		File[] files = file.listFiles();
-		
+
 		for(File f: files) {
 			if (f.isDirectory()) {
 				System.out.println("Dir "+f.getName());
@@ -21,40 +24,40 @@ public class Terminal {
 			}
 		}
 	}
-	
+
 	public static File cd(File ex_file, String name) {
 		File new_file = new File(ex_file, name);
 		if (!new_file.exists()) {
-			System.out.println("cd: no such file or directory: "+name);
+			System.out.println(NO_SUCH_DIRECTORY+name);
 			return ex_file;
 		}
-		
+
 		return new_file;
 	}
-	
+
 	public static File cdToBefore(File file){
 		File parent = file.getParentFile();
-		
+
 		return parent;
 	}
-	
+
 	public static void mkdir(File file, String name) {
 		File new_file = new File(file, name);
 		new_file.mkdirs();
 		return;
 	}
-	
+
 	public static void copyFile(File dest, File source) {
 		FileInputStream in = null;
 		FileOutputStream out = null;
-		
+
 		try {
 			byte[] arr = new byte[500];
 			int count = 0;
-			
+
 			in = new FileInputStream(source);
 			out = new FileOutputStream(dest);
-			
+
 			while((count = in.read(arr)) != -1) {
 				out.write(arr);
 				Arrays.fill(arr, (byte)0);
@@ -70,10 +73,10 @@ public class Terminal {
 			}
 		}
 	}
-	
+
 	public static void cp(File dest, File source) {
 		File[] sourceFiles = source.listFiles();
-		
+
 		for(File sf: sourceFiles) {
 			File newDest = new File(dest, sf.getName());
 			if (sf.isDirectory()) {
@@ -86,41 +89,44 @@ public class Terminal {
 	}
 
 	public static void main(String[] args) {
-		String path = "{$시작 위치}";
-		File file = new File(path);
-		
+		File file = new File(HOME);
+
 		while(true) {
 			System.out.print("> ");
 			String command = SCANNER.nextLine();
 			String[] commands = command.split(" ");
-			
+
 			switch(commands[0]) {
-			case "pwd":
-				System.out.println(file.getPath());
-				break;
-			case "ls":
-				ls(file);
-				break;
-			case "cd":
-				if(commands[1].matches("^[\\w][-_.\\\\|\\w]*")) {
-					file = cd(file, commands[1]);
-				} else if (commands[1].equals("..")) {
-					file = cdToBefore(file);
-				} else {
-					System.out.println("cd: no such file or directory: "+commands[1]);
-				}
-				break;
-			case "mkdir":
-				mkdir(file, commands[1]);
-				break;
-			case "cp":
-				File sourceFiles = new File(commands[1]);
-				cp(file, sourceFiles);
-				break;
-			case "exit":
-				return;
-			default:
-				System.out.println("zsh: command not found: "+commands[0]);
+				case "pwd":
+					System.out.println(file.getPath());
+					break;
+				case "ls":
+					ls(file);
+					break;
+				case "cd":
+					if (commands.length < 2) {
+						file = new File(HOME);
+						break;
+					}
+					if(commands[1].matches("^[\\w][-_.\\\\|\\w]*")) {
+						file = cd(file, commands[1]);
+					} else if (commands[1].equals("..")) {
+						file = cdToBefore(file);
+					} else {
+						System.out.println(NO_SUCH_DIRECTORY+commands[1]);
+					}
+					break;
+				case "mkdir":
+					mkdir(file, commands[1]);
+					break;
+				case "cp":
+					File sourceFiles = new File(commands[1]);
+					cp(file, sourceFiles);
+					break;
+				case "exit":
+					return;
+				default:
+					System.out.println(NO_SUCH_COMMAND+commands[0]);
 			}
 		}
 	}
