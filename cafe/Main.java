a3f36749734637cea8c697563300d590cd485d2a
bahar-j <joonie811@naver.com>
2021-02-16 15:07:21 +0900

    fix: the problem saving coupon data

--- cafe/Main.java
+++ cafe/Main.java
@@ -1,14 +1,10 @@
 package kosta.cafe;
 
-
-import java.util.Scanner;
-
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO: 상담원 채팅, 직원 여러명
 		CustomerManager customerManager = new CustomerManager();
-		Scanner scanner = new Scanner(System.in);
 		Employee employee = new Employee();
 		employee.start();
 
@@ -18,7 +14,7 @@ public class Main {
 			System.out.println("어서오세요:)");
 			System.out.println("1.회원가입  2.로그인 3.종료");
 			System.out.print("원하시는 메뉴 선택해주세요: ");
-			String menu = scanner.nextLine();
+			String menu = Util.SCANNER.nextLine();
 
 			if (menu.equals("1")) {
 				customerManager.join();
@@ -27,21 +23,21 @@ public class Main {
 				while(true){
 					System.out.println("무엇을 도와드릴까요?");
 					System.out.println("1.주문하기 2.선호메뉴 등록 3.종료");
-					String num = scanner.nextLine();
+					String num = Util.SCANNER.nextLine();
 					if(num.equals("1")) {
-						if(customerManager.loggedInCustomer.getCoupon()>=5) {
-							employee.useCoupon(customerManager.loggedInCustomer);
+						if(customerManager.getLoggedInCustomer().getCoupon()>=5) {
+							employee.useCoupon(customerManager.getLoggedInCustomer());
 							continue;
 						}
-						if (customerManager.loggedInCustomer.getLiked()!=null) {
-							employee.useLiked(customerManager.loggedInCustomer);
+						if (customerManager.getLoggedInCustomer().getLiked()!=null) {
+							employee.useLiked(customerManager.getLoggedInCustomer());
 						} else {
-							employee.getOrder(customerManager.loggedInCustomer);
+							employee.getOrder(customerManager.getLoggedInCustomer());
 						}
 					} else if (num.equals("2")) {
 						employee.showMenu();
 						System.out.print("메뉴: ");
-						String coffeeName = scanner.nextLine();
+						String coffeeName = Util.SCANNER.nextLine();
 						Coffee coffee = employee.grabCoffee(coffeeName);
 						if(coffee != null) {
 							customerManager.addLiked(coffee);
@@ -50,6 +46,7 @@ public class Main {
 						}
 					} else if (num.equals("3")) {
 						System.out.println("프로그램 종료");
+						customerManager.updateCurrentUserData();
 						return;
 					} else {
 						System.out.println("없는 메뉴입니다. 다시 시도하세요.");
@@ -57,6 +54,7 @@ public class Main {
 				}
 			} else if (menu.equals("3")) {
 				System.out.println("프로그램 종료");
+				customerManager.updateCurrentUserData();
 				return;
 			}else {
 				System.out.println("없는 메뉴입니다. 다시 시도하세요.");
